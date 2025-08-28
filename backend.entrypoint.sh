909ae2254268214940707df7d47731494d53416f
Martin Bock <martinbock@arcor.de>
2025-08-28 16:21:12 +0200

    Update gunicorn command to remove --reload option
    Removed the --reload option from gunicorn command.

--- backend.entrypoint.sh
+++ backend.entrypoint.sh
@@ -40,7 +40,7 @@ EOF
 
 python manage.py rqworker default &
 
-exec gunicorn core.wsgi:application --bind 0.0.0.0:8000 --reload --timeout 300 # --reload Nur für die Entwicklungszeit
+exec gunicorn core.wsgi:application --bind 0.0.0.0:8000 --timeout 300 # --reload Nur für die Entwicklungszeit
 
 # Nicht im Live-Betriebnutzen!
 # exec python manage.py runserver 0.0.0.0:8000
